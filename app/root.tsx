b01a9252b8d83693ddebd584b0e39a9719dcdc28
Cillian Barron <barroncillian@gmail.com>
2025-04-08 20:25:52 +0100

    feat: clean up welcome route

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="min-h-svh">
         {children}
         <ScrollRestoration />
         <Scripts />
